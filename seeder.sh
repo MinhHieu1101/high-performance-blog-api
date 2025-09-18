4aaf7f1fdb9700237f7b6c1ca4ef814203be863e
MinhHieu1101 <sieubanphao@gmail.com>
2025-09-19 03:42:52 +0700

    remove jq to avoid error for seeder script

--- seeder.sh
+++ seeder.sh
@@ -2,8 +2,16 @@
 set -e
 BASE="http://localhost:8080"
 
-curl -s -X POST "$BASE/posts" -H "Content-Type: application/json" -d '{"title":"Go tips","content":"Tips for Go developers","tags":["go","programming"]}' | jq || true
-curl -s -X POST "$BASE/posts" -H "Content-Type: application/json" -d '{"title":"Docker trick","content":"How to use docker-compose","tags":["devops","docker"]}' | jq || true
-curl -s -X POST "$BASE/posts" -H "Content-Type: application/json" -d '{"title":"Testing in Go","content":"httptest and best practices","tags":["go","testing"]}' | jq || true
+curl -s -X POST "$BASE/posts" \
+  -H "Content-Type: application/json" \
+  -d '{"title":"Go tips","content":"Tips for Go developers","tags":["go","programming"]}'
 
-echo "seeded posts"
+curl -s -X POST "$BASE/posts" \
+  -H "Content-Type: application/json" \
+  -d '{"title":"Docker trick","content":"How to use docker-compose","tags":["devops","docker"]}'
+
+curl -s -X POST "$BASE/posts" \
+  -H "Content-Type: application/json" \
+  -d '{"title":"Testing in Go","content":"httptest and best practices","tags":["go","testing"]}'
+
+echo -e "\nseeded posts"
